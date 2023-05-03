7436203cee9a16077e004d5c733b80100f406592
melckzedeck <melckzedeck063@gmail.com>
2023-05-03 12:55:54 +0300

    new commit  with modified server urls

--- src/store/urls.js
+++ src/store/urls.js
@@ -1,7 +1,7 @@
-export const BASE_URL =  "http://192.168.43.33:4501/api/v1";
-export const AUTH_URL = "http://192.168.43.33:4501/api/v1/user";
+// export const BASE_URL =  "http://192.168.43.33:4501/api/v1";
+// export const AUTH_URL = "http://192.168.43.33:4501/api/v1/user";
 
 
 
-// export const BASE_URL =  "http://localhost:4501/api/v1";
-// export const AUTH_URL = "http://localhost:4501/api/v1/user";
+export const BASE_URL =  "http://localhost:4501/api/v1";
+export const AUTH_URL = "http://localhost:4501/api/v1/user";
